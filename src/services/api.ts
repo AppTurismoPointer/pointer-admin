f9ad4e6af7ecbe19c3042c3d8de99e31b00fc4dd
Brenda Matias <brendamatias.sobral@gmail.com>
2024-05-30 20:04:22 -0300

    chore: change url api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://pointer-api-wew4.onrender.com/admin",
-  baseURL: "http://localhost:3333/admin",
+  baseURL: "https://pointer-api-wew4.onrender.com/admin",
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
